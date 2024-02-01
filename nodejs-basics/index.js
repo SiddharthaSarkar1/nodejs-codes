4f68c1fab4f56785122b4083aaac88214224e2c4
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-02 00:50:03 +0530

    Initial commit

---
+++ nodejs-basics/index.js
@@ -0,0 +1,7 @@
+console.log("This is NodeJS tutorial for beginners.");
+console.log("This is NodeJS tutorial for beginners.");
+console.log("This is NodeJS tutorial for beginners.");
+
+// process.exit(1);
+// process.exit = 1;
+// process.exitCode = 1;
